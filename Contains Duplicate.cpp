b3402c91c259dbed5331d9fb412bac0690e955d1
RANJIT TIWARI <65909835+Ranjit85@users.noreply.github.com>
2022-11-03 14:27:30 +0530

    Create Contains Duplicate.cpp

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp[nums[i]]>=2)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+};
